3aa8910113b4e865a373557e33daa52561f8b47e
Gabriela Suzana <gaby19812009@gmail.com>
2026-07-02 14:20:35 -0300

    Deploy 3

--- src/main/java/br/com/myapp/HomeController.java
+++ src/main/java/br/com/myapp/HomeController.java
@@ -13,7 +13,7 @@ public class HomeController {
         <body style="font-family:Arial;text-align:center;margin-top:100px;">
             <h1>Minha Aplicação Web</h1>
             <h2>Deploy 1</h2>
-            <p>Terceira versão publicada automaticamente pelo Jenkins e Docker.</p>
+            <p>Primeira versão publicada automaticamente pelo Jenkins e Docker.</p>
         </body>
         </html>
         """;
